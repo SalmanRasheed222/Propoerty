d287a9b72c35052ee817460604f18bbaa8fe3dbf
SALMAN <SALMAN@DESKTOP-Q0BOMET>
2022-09-21 23:03:18 +0500

    Add project files.

---
+++ Propoerty/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+using Propoerty;
+
+student s1 = new student();
+student s2 = new student();
+student s3 = new student();
+student max = new student();
+
+
+Console.WriteLine("enter name");
+s1.name = (Console.ReadLine());
+Console.WriteLine("enter number");
+s1.number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter name");
+s2.name = (Console.ReadLine());
+Console.WriteLine("enter number");
+s2.number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter name");
+s3.name = (Console.ReadLine());
+Console.WriteLine("enter number");
+s3.number = Convert.ToInt32(Console.ReadLine());
+
+max = s1;
+if (s2.number > max.number)
+{
+	max = s2;
+}
+if (s3.number > max.number)
+{
+	max = s3;
+}
+
+Console.WriteLine("student name is:" + max.name);
+Console.WriteLine("number is:" + max.number);
---
+++ Propoerty/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propoerty
+{
+	public class student
+	{
+		public string name { set; get; }
+		public int number { get; set; }
+	}
+
+
+
+
+
+
+}
